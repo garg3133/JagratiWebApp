6e4b9ecebab45c0a582dcd16b4b23f418d94bb2f
Priyansh Garg <priyanshgarg30@gmail.com>
2019-11-23 01:35:53 +0530

    dashboard-basic-layout

--- Jagrati/home/context_processors.py
+++ Jagrati/home/context_processors.py
@@ -1,6 +1,37 @@
-# import ...
+from .models import(
+	Volunteer,
+	Volunteer_schedule,
+	Volunteer_attended_on,
+	Student,
+	Student_schedule,
+	Student_attended_on,
+	Calendar,
+	Schedule,
+	cw_hw,
+) 
 
-# def categories(request):
-#     return {
-#         'categories': Categories.objects.all()
-#     }
+def database_context(request):
+	if request.user.is_authenticated:
+		# Logged in Volunteer's data
+		obj = Volunteer.objects.filter(email = request.user)
+		if obj.exists():
+			volunteer = obj[0]
+			obj2 = Volunteer_schedule.objects.filter(roll_no = volunteer)
+			if obj2.exists():
+				vol_schedule = obj2[0]
+			else:
+				vol_schedule = None
+		else:
+			volunteer = None
+			vol_schedule = None
+
+
+		return {
+			'volunteer': volunteer,
+			'volunteers': Volunteer.objects.all(),
+			'vol_schedule': vol_schedule,
+			'vol_schedules': Volunteer_schedule.objects.all(),
+		}
+
+	else:
+		return {}
