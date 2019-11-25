a17763f0d1c6203d7098c75977a6a3083c5af825
Priyansh Garg <priyanshgarg30@gmail.com>
2019-11-25 13:59:05 +0530

    dashboard update complete

--- Jagrati/home/views.py
+++ Jagrati/home/views.py
@@ -51,16 +51,21 @@ def dashboard(request):
 							cw_hw = Cw_hw.objects.filter(date = class_info_date, section = schedule[0])
 							if cw_hw.exists():
 								context = {
+									#dash-main
 									'schedule' : schedule,
 									'students_attended' : students_attended,
 									'cw_hw' : cw_hw[0],
 									'selected_date' : class_info_date,
 									'selected_schedule' : class_info_section,
 									'choices': Schedule.SECTION,
+
+									#dash-update
+									'last_4_year': datetime.now().year - 4,
 								}
 								return render(request, 'home/dashboard.html', context)
 							else:
 								context = {
+									#dash-main
 									'schedule' : schedule,
 									'students_attended' : students_attended,
 									'cw_hw' : {
@@ -70,31 +75,46 @@ def dashboard(request):
 									'selected_date' : class_info_date,
 									'selected_schedule' : class_info_section,
 									'choices': Schedule.SECTION,
+
+									#dash-update
+									'last_4_year': datetime.now().year - 4,
 								}
 								return render(request, 'home/dashboard.html', context)
 						else:
 							context = {
+								#dash-main
 								'no_schedule_found' : "yup!",
 								'selected_date' : class_info_date,
 								'selected_schedule' : class_info_section,
 								'choices': Schedule.SECTION,
+
+								#dash-update
+								'last_4_year': datetime.now().year - 4,
 							}
 							return render(request, 'home/dashboard.html', context)  # The chosen section is not taught on the chosen day
 					else:
 						context = {
+							#dash-main
 							'calendar' : calendar[0],
 							'no_class_scheduled' : "haan",
 							'selected_date' : class_info_date,
 							'selected_schedule' : class_info_section,
 							'choices': Schedule.SECTION,
+
+							#dash-update
+							'last_4_year': datetime.now().year - 4,
 						}
 						return render(request, 'home/dashboard.html', context)
 				else:
 					context = {
+						#dash-main
 						'no_class_found' : "bilkul_nhi",
 						'selected_date' : class_info_date,
 						'selected_schedule' : class_info_section,
 						'choices': Schedule.SECTION,
+
+						#dash-update
+						'last_4_year': datetime.now().year - 4,
 					}
 					return render(request, 'home/dashboard.html', context)
 			elif request.POST.get('submit') == 'update-profile':
@@ -113,20 +133,63 @@ def dashboard(request):
 				dob             = request.POST['dob']
 				contact_no      = request.POST['contact_no']
 
+				volun = Volunteer.objects.get(email = request.user)
+				update_error = ""
+				toast = ""
 				if roll_no:
-					if Volunteer.objects.get(email = request.user).roll_no != roll_no:
-						volun = Volunteer.objects.get(email = request.user)
-						volun.roll_no = roll_no
-						volun.save()
-					else:
-						update_error = "A volunteer with entered roll no. already exists."
+					if volun.roll_no != roll_no:
+						duplicate_roll_check = Volunteer.objects.filter(roll_no = roll_no)
+						if duplicate_roll_check.exists():
+							update_error = "A volunteer with entered roll no. already exists."
+							toast = "Profile update failed!"
+						else:
+							volun.roll_no = roll_no
+				if first_name:
+					volun.first_name = first_name
+				if last_name:
+					volun.last_name = last_name
+				volun.gender = gender
+				volun.batch = batch
+				volun.programme = programme
+				volun.dob = dob
+				if contact_no:
+					volun.contact_no = contact_no
+				if alt_email:
+					volun.alt_email = alt_email
+				if street_address1:
+					volun.street_address1 = street_address1
+				if street_address2:
+					volun.street_address2 = street_address2
+				if city:
+					volun.city = city
+				if state:
+					volun.state = state
+				if pincode:
+					volun.pincode = pincode
+
+				if update_error == "":
+					volun.save()
+					toast = "Profile updated Successfully!"
 
 
-				return render(request, 'home/dashboard.html')
+				context = {
+					#dash-main
+					'class_info_submitted' : "nooooo!",
+					'choices': Schedule.SECTION,
+
+					#dash-update
+					'update_error' : update_error,
+					'last_4_year': datetime.now().year - 4,
+					'toast': toast,
+				}
+				return render(request, 'home/dashboard.html', context)
 		else:
 			context = {
-				'submitted' : "nopes",  # Nothing submitted if this variable exists
+				#dash-main
+				'class_info_submitted' : "nopes",
 				'choices': Schedule.SECTION,
+
+				#dash-update
 				'last_4_year': datetime.now().year - 4,
 			}
 			return render(request, 'home/dashboard.html', context)
