e3de06d43bc69011562e71acd71feb6f4f5b50f6
Priyansh Garg <priyanshgarg30@gmail.com>
2020-07-10 13:01:15 +0530

    login-signup-logout views refactored

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
@@ -6,8 +7,11 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
+from django.utils.html import strip_tags
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .tokens import account_activation_token
+
+from home.models import Volunteer
 
 # from django.contrib.auth.views import PasswordResetConfirmView
 # from .forms import CustomSetPasswordForm
@@ -21,31 +25,41 @@ def login_signup(request):
     if request.user.is_authenticated:
         return redirect('home')
     
-    next_site = 'dashboard'
-    if request.GET.get('next') is not None:
-        next_site = request.GET['next']
-        print(next_site)
-    
+    # next_site = 'dashboard'
+    # if request.GET.get('next') is not None:
+    #     next_site = request.GET['next']
+    #     print(next_site)
+    next_site = request.GET.get('next', 'dashboard')
+
     if request.method == 'POST':
         if request.POST.get('submit') == 'login':
             email = request.POST['email']
             password = request.POST['password']
-            
-            obj = User.objects.filter(email=email)
-            if obj.exists() and not obj[0].is_active:
-                context = {
-                    'login_error' : "Confirm your mail first.",
-                }
-                return render(request, 'accounts/login_signup.html', context)  # Must open other html page with option to resend mail
 
+            context = {}
             user = authenticate(username = email, password = password)
+
             if user is not None:
+                volun = Volunteer.objects.filter(email=user)
+                if not user.auth and volun.exists():
+                    # If User has already completed the profile
+                    # but is not yet verified by the admin
+                    context['login_error'] = 'User is not yet authenticated by the Admin. Kindly contact Admin.'
+                    return render(request, 'accounts/login_signup.html', context)
+
                 login(request, user)
-                return redirect(next_site)
+
+                if not user.auth:
+                    return redirect('set_profile')
+                else:
+                    return redirect(next_site)
             else:
-                context = {
-                    'login_error' : "Invalid Username or Password",
-                }
+                user = User.objects.filter(email=email)
+                if user.exists() and user[0].check_password(password) and not user[0].is_active:
+                    # Authentication failed because user is not active
+                    context['login_error'] = 'Account not Activated.<br><a href="#">Resend Activation Email?</a>'
+                else:
+                    context['login_error'] = 'Invalid credentials'
                 return render(request, 'accounts/login_signup.html', context)
 
             # form = LoginForm(request.POST)
@@ -63,34 +77,37 @@ def login_signup(request):
             password2 = request.POST['password2']
 
             error = ''
-            obj = User.objects.filter(email=email)
-            if obj.exists():
+            user = User.objects.filter(email=email)
+            if user.exists():
                 error = "Account with entered email already exists"
                 return render(request, "accounts/login_signup.html", {'signup_error' : error})
             if password1 and password2 and password1 != password2:
                 error = "Passwords don't match"
                 return render(request, "accounts/login_signup.html", {'signup_error' : error})
 
-            obj = User(email = email, desig = desig, is_active = False) #ADD IS_ACTIVE=FALSE AT TIME OF EMAIL CONFIRMATION
-            obj.set_password(password1) # To save password as hash
-            obj.save()
+            user = User(email=email, desig=desig, is_active=False)
+            user.set_password(password1) # To save password as hash
+            user.save()
 
+            # Send Account Activation Email
             current_site = get_current_site(request)
+
+            from_email = settings.DEFAULT_FROM_EMAIL
+            to = [email]
+            subject = 'Jagrati Acount Activation'
             html_message = render_to_string('accounts/account_activation_email.html', {
-                'user': obj,
+                'user': user,
                 'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(obj.pk)),
-                'token': account_activation_token.make_token(obj),
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
             })
+            plain_message = strip_tags(html_message)
             send_mail(
-                'Acount Activation',
-                'Activate your account',
-                'jagrati123321@gmail.com',
-                [email],
-                fail_silently=False,
-                html_message = html_message,
+                subject, plain_message, from_email, to,
+                fail_silently=False, html_message=html_message,
             )
-            return render(request, 'accounts/activation_mail_sent.html', {'user' : obj,})
+
+            return render(request, 'accounts/activation_mail_sent.html', {'user' : user,})
             # user = authenticate(username=email, password=password1)
             # login(request, user)
             # return redirect('home')
@@ -109,7 +126,7 @@ def login_signup(request):
     return render(request, 'accounts/login_signup.html')
 
 def logout_view(request):
-    next_site = request.GET['next']
+    next_site = request.GET.get('next', 'home')
     logout(request)
     return redirect(next_site)
 
