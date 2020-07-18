8a07c10f699d5cf56f409284ded8fc66f429e7b1
Priyansh Garg <priyanshgarg30@gmail.com>
2020-07-18 14:52:22 +0530

    created profile model

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 
@@ -14,51 +16,46 @@ class UserManager(BaseUserManager):
             raise ValueError("User must have an email address")
         if not password:
             raise ValueError("User must have a password")
-        user_obj = self.model(
-            email = self.normalize_email(email),
+        user = self.model(
+            email=self.normalize_email(email),
         )
-        user_obj.set_password(password)
-        user_obj.save(using = self._db)
-        return user_obj
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
     def create_staffuser(self, email, password=None):
         user = self.create_user(
             email,
-            password = password,
+            password=password,
         )
         user.is_staff = True
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
     
     def create_superuser(self, email, password=None):
         user = self.create_user(
             email,
-            password = password,
+            password=password,
         )
         user.is_staff = True
         user.is_superuser = True
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     DESIG = (
-        ('V', 'Volunteer'),
-        ('F', 'Faculty')
+        ('v', 'Volunteer'),
+        ('f', 'Faculty')
     )
 
-    email                   = models.EmailField(verbose_name = "email address", max_length = 255, unique = True,)
-    date_joined             = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login              = models.DateTimeField(verbose_name='last login', auto_now=True)
-    # is_admin                = models.BooleanField(default=False)
-    # is_admin is used if not using Permissions, is_superuser if using it.
-    is_active               = models.BooleanField(default=True)
-    is_staff                = models.BooleanField(default=False)
-    # is_superuser            = models.BooleanField(default=False)
-    # is_superuser already available by PermissionsMixin
-    desig                   = models.CharField(max_length = 1, choices = DESIG, default = 'V')
-    auth                    = models.BooleanField(default = False)  # For authentication by admin
-    # active                  = models.BooleanField(default = True)   # Will continue or on pause  (WILL PUT IT IN VOLUN TABLE)
+    email = models.EmailField(verbose_name="Email Address", max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    auth = models.BooleanField(default=False)
+    desig = models.CharField(max_length=1, choices=DESIG, default='v')
+    date_joined = models.DateTimeField(verbose_name='Date Joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='Last Login', auto_now=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []   # Required for 'createsuperuser'
@@ -77,11 +74,38 @@ class User(AbstractBaseUser, PermissionsMixin):
 
         super(User, self).save(*args, **kwargs)
 
-
-
-
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     """Generate a Token everytime a new User registers."""
     if created:
         Token.objects.create(user=instance)
+
+
+class Profile(models.Model):
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(verbose_name="First Name",max_length=50)
+    last_name = models.CharField(verbose_name="Last name",max_length=50)
+    profile_image = ProcessedImageField(
+        upload_to='profile_pics', processors=[ResizeToFill(300,300)],
+        format='JPEG', options={'quality': 60}, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER)
+    alt_email = models.EmailField(verbose_name="Alternate Email", max_length=255, blank=True)
+    contact_no = models.CharField(verbose_name="Contact Number", max_length=13)
+    street_address1 = models.CharField(verbose_name="Address Line 1", max_length=255)
+    street_address2 = models.CharField(verbose_name="Address Line 2", max_length=255, blank=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    pincode = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f'{self.get_full_name} - {self.user.email}'
+
+    @property
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
