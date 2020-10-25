eafd953c94924366d16cfe8d886e49b6652f66fe
dynamoh <guptaheet53@gmail.com>
2020-10-25 20:52:28 +0530

    Configured manage.py for Production

--- manage.py
+++ manage.py
@@ -2,15 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from decouple import config
 
+PRODUCTION = (config("PRODUCTION") == 'True')
 
 def main():
-    """
-        for production use this :
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jagrati.settings.production') 
-    """
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jagrati.settings.development') 
+    if PRODUCTION:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jagrati.settings.production') 
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jagrati.settings.development') 
+        
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
