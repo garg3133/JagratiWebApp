5043c7c4aa1d988bc380d12e2bc12bf84e4a3aa6
Tushar Maheshwari <tusharmaheshwaritm6@gmail.com>
2021-01-02 15:41:06 +0530

    Old Home Page is set to /old

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index_new, name='index_new'),
+    path('old/', views.index, name='index_old'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/update_cwhw/', views.update_cwhw, name='update_cwhw'),
     path('ajax/dashboard/', views.ajax_dashboard, name='ajax_dashboard'),
