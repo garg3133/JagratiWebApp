8a5369b837e184aa0c5ee5bb21339bbb149e1579
Tushar Maheshwari <tusharmaheshwaritm6@gmail.com>
2021-01-02 15:40:44 +0530

    Initivatives Added

--- apps/misc/models.py
+++ apps/misc/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
+class Initiative(models.Model):
+
+	title = models.CharField(max_length = 20, verbose_name = 'Initiative Name')
+	description = models.CharField(max_length = 200, verbose_name = 'Initiative Discription')
+	thumbnail = models.ImageField(null = True, upload_to = 'initiative/')
+
+	def __str__(self):
+		return self.title
