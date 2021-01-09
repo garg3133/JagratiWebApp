b0400e0d604ba64e337ce8e8cbd86832c5bd257a
Jitendra Kumar <jitendrar048@gmail.com>
2021-01-09 13:11:37 +0530

    [update]: url to '/' and reverse name to 'volunteers'

--- apps/volunteers/urls.py
+++ apps/volunteers/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'volunteers'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.volunteers, name='volunteers'),
     path('profile/<int:pk>/', views.profile, name='profile'),
     path('attendance/', views.attendance, name='attendance'),
     # path('attendance/view/', views.view_attendance, name='view_attendance'),
@@ -15,5 +15,4 @@ urlpatterns = [
     path('profile/update/', views.update_profile, name='update_profile'),  # Not needed, update in profile/ only
     path('list/', views.volunteers_list, name='volunteers_list'),
     path('ajax/list/', views.ajax_volunteers_list, name='ajax_volunteers_list'),
-    path('all_volunteers/', views.VolunteerListView.as_view(), name='all_volunteers'),
 ]
