5c5f3b2e62dbce693d7bdf00a433be75df835999
Priyansh Garg <priyanshgarg30@gmail.com>
2021-01-29 16:45:40 +0530

    Fix broken links to password change/reset pages.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 
 from django.contrib.auth import views as auth_views
@@ -16,11 +16,17 @@ urlpatterns = [
     path('authenticated/', views.account_authenticated, name='account_authenticated'),
 
     # Django Auth URLs
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('accounts:password_change_done')),
+         name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')),
+         name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete')),
+         name='password_reset_confirm'),
     # path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
