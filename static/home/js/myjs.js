9c84f4b4c07cc793d9e478037cb125b92f2fdbd8
Priyansh Garg <priyanshgarg30@gmail.com>
2021-03-17 00:30:13 +0530

    Rebuild homepage navbar.

--- static/home/js/myjs.js
+++ static/home/js/myjs.js
@@ -1,58 +1,24 @@
-//Nav bar variables
-const nav_height= "154px";
-
 //Nav button controls
+const navbar_toggler = document.querySelector('.navbar-toggler i');
+const left_nav = document.querySelector('.navbar .left-nav .navbar-nav');
 
-function nav_button(){
-	var a = document.getElementById("inavbar2");
-	a.style.padding="2px 5px 5px";
-	a.style.height=nav_height;
-	var a = document.getElementById("inav-button-close");
-	a.style.display="block";
-	var a = document.getElementById("inav-button");
-	a.style.display="none";
-}
-function nav_button_close(){
-	var a = document.getElementById("inavbar2");
-	a.style.padding="0";
-	a.style.height="0";
-	var a = document.getElementById("inav-button-close");
-	a.style.display="none";
-	var a = document.getElementById("inav-button");
-	a.style.display="block";
-}
-function for_media_query(x){
-	if (x.matches){
-		document.getElementById("inavbar2").style.padding="0";
-		document.getElementById("inavbar2").style.height="0px";
-		document.getElementById("inav-button").style.display="block";
-		document.getElementById("inav-button-close").style.display="none";
-
-		document.querySelector(".disable_arrows").style.display="none";
-	}
-	else{
-		document.getElementById("inavbar2").style.padding="0";
-		document.getElementById("inavbar2").style.height="0px";
-		document.getElementById("inav-button").style.display="none";
-		document.getElementById("inav-button-close").style.display="none";
-
-		document.querySelector(".disable_arrows").style.display="block";
-	}
+function toggleNav() {
+	navbar_toggler.classList.toggle('fa-bars');
+	navbar_toggler.classList.toggle('fa-times');
+	left_nav.classList.toggle('nav-collapsed');
 }
 
-var x=window.matchMedia("(max-width:760px)");
-for_media_query(x);
-x.addListener(for_media_query);  /*Whenever value of x is changed, for_media_query is called.*/
-
+navbar_toggler.addEventListener('click', function() {
+	toggleNav();
+});
 
 
 window.onclick = function(e){
 	// Navbar closes if clicks anywhere else
-	if(!(e.target.matches('.navbut') || e.target.matches('.menu-icon'))){
-		if(document.getElementById("inavbar2").style.height==="154px"){
-			nav_button_close();
-		}
+	if (!e.target.closest('.navbar')) {
+		if (navbar_toggler.classList.contains('fa-times')) toggleNav();
 	}
+
 	// Donate us modal close
 	if(e.target == don_modal){
 		don_modal.style.display="none";
@@ -67,17 +33,6 @@ function donate_us(){
 const modal_cl = document.getElementById("don-close");
 modal_cl.onclick = function(){
 	don_modal.style.display="none";
-}
-
-//Google Map API
-function myMap() {
-	var mapProp= {
-	  center:new google.maps.LatLng(23.1782409,80.0252362),
-	  zoom:15,
-	};
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	var marker = new google.maps.Marker({position: mapProp.center});
-	marker.setMap(map); 
 }
 
 // SHOWCASE TEXT
