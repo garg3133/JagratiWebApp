3ff4272b48b70258de01bd170335cc94779ddcb7
Sanjana Raghuwanshi <raghuneha2803.com>
2021-03-31 23:26:46 +0530

    new_addition2

--- apps/events/views.py
+++ apps/events/views.py
@@ -17,9 +17,9 @@ def add_event(request):
         schedule = request.POST['schedule']
         venue = request.POST['venue']
         thumbnail = request.FILES.get('thumbnail')
-        
+
         event = Event(
-            title=title, schedule=schedule, venue=venue, description=description, thumbnail=thumbnail, 
+            title=title, schedule=schedule, venue=venue, description=description, thumbnail=thumbnail,
         )
         event.save()
 
