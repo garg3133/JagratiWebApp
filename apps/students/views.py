25d37bc06b760ba1961c481e3245c9148785e35d
Satyam Kulkarni <sk1122@localhost.localdomain>
2021-03-31 09:39:40 +0530

    Added HomeWork Toggle Button

--- apps/students/views.py
+++ apps/students/views.py
@@ -218,7 +218,8 @@ def ajax_mark_homework(request):
         stu_id = request.POST.get('stu_id')
         is_homework_done = request.POST.get('is_homework_done')
         todayDate = request.POST.get('date')
-        todayDate = datetime.strptime(todayDate, "%a, %d %b %Y %H:%M:%S %Z")
+        todayDate = datetime.strptime(todayDate, "%Y-%m-%d").date()
+        todayDate = Calendar.objects.get(date=todayDate)
         student = StudentAttendance.objects.get(
             student__id=stu_id, cal_date=todayDate)
         student.hw_done = True if is_homework_done == 'true' else False
