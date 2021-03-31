9b239a72acb7eea3348401be673f0de2bba77846
Sanjana Raghuwanshi <raghuneha2803.com>
2021-03-31 11:16:26 +0530

    add_event_page4

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,24 +1,25 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Event
-from django.contrib import messages
+from django.contrib import message
+
 
 def index(request):
     return HttpResponse('Hello World!')
 
+
 @permission_required('events.add_event', raise_exception=True)
 def add_event(request):
-	# Add new event
     if request.method == 'POST':
-        title = request.POST.get('title')
+        title = request.POST['title']
         schedule = request.POST.get('schedule')
         venue = request.POST['venue']
         description = request.POST['description']
         thumbnail = request.FILES.get('thumbnail')
 
         event = Event(
-            title=title,schedule=schedule, venue=venue, description=description, thumbnail=thumbnail,
+            title=title, schedule=schedule, venue=venue, description=description, thumbnail=thumbnail,
         )
         event.save()
 
