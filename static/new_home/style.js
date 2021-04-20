2f86d41010d159d330e3002ec1c88801f375fb3c
cryptofox17 <cryptofoxinc@gmail.com>
2021-04-20 20:04:54 +0530

    used bootstrap modal

--- static/new_home/style.js
+++ static/new_home/style.js
@@ -22,21 +22,4 @@ $(window).scroll(function() {
 });
 
 
-window.onclick = function(e){
-	// Donate us modal close
-	if(e.target == don_modal){
-		don_modal.style.display="none";
-	}
-}
-
-// Donate Us Modal Close
-const don_modal = document.getElementById("don-modal");
-function donate_us(){
-	don_modal.style.display="block";
-}
-const modal_cl = document.getElementById("don-close");
-modal_cl.onclick = function(){
-	don_modal.style.display="none";
-}
-
 
