7ee7c4931253c8f54e51eea3ec3df7bfb6fc38b9
Priyansh Garg <priyanshgarg30@gmail.com>
2022-05-03 20:48:28 +0530

    Setup for main production.

--- Jagrati/settings/production.py
+++ Jagrati/settings/production.py
@@ -3,7 +3,7 @@
 from .base import *
 import ast
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY_JAGRATI')
 
 DEBUG = config("DEBUG", cast=bool)
 
@@ -14,14 +14,8 @@ ALLOWED_HOSTS = ast.literal_eval(config("ALLOWED_HOSTS"))
 
 DATABASES = {
     'default': {
-        'NAME': config("DB_NAME"),
-        'ENGINE': 'mysql.connector.django',
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASS"),
-        'HOST': config("DB_HOST"),
-        'OPTIONS': {
-            'autocommit': True,
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -29,12 +23,11 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
-# EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool)
-EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_PORT = 587
 
 DEFAULT_FROM_EMAIL = config("SENDER_EMAIL")
 ADMINS_EMAIL = ast.literal_eval(config("ADMINS_EMAIL"))
@@ -43,12 +36,12 @@ ADMINS_EMAIL = ast.literal_eval(config("ADMINS_EMAIL"))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = config("STATIC_ROOT")
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'staticfiles/')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = config("MEDIA_ROOT")
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media/')
 MEDIA_URL = '/media/'
 
-TEMP_ROOT = config("TEMP_ROOT")
+TEMP_ROOT = os.path.join(BASE_DIR, '..', 'temp/')
 
 STATICFILES_DIRS = [BASE_DIR+"/static", ]
